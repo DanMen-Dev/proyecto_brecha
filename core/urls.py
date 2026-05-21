6720bdfd7127aff953ba7fb8de72d5aa6fed59b5
daniel <dani23000@gmail.com>
2026-05-20 21:12:10 -0400

    Fix: Urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include # Agregamos 'include' para los usuarios
 from exchange.views import home, dashboard, lista_precios_cashea, register, exportar_precios_pdf, cargar_csv, actualizar_tasas_manual, solicitar_pro 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve # Librería nativa para servir en producción
+from django.urls import re_path
+ro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,5 +20,8 @@ urlpatterns = [
     path('cargar-csv/', cargar_csv, name='cargar_csv'),
     path('actualizar-tasas/', actualizar_tasas_manual, name='actualizar_tasas'),
     path('solicitar-pro/', solicitar_pro, name='solicitar_pro'),
+
+    # EL PASAPORTE DE ENTRADA INDUSTRIAL PARA LOS ESTÁTICOS CON DEBUG=FALSE:
+    re_path(r'^static/(?p<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
